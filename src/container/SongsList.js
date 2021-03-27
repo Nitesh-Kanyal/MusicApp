e4b3ca2652d099628d8a0f377af20e940b5f0704
Nitesh Kanyal <liam@vex-fitness.com>
2021-03-27 14:20:33 +0530

    removing unwanted code

--- src/container/SongsList.js
+++ src/container/SongsList.js
@@ -1,81 +1,84 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
-  View,
-  Text,
-  StyleSheet,
-  FlatList,
-  ActivityIndicator,
-} from 'react-native';
-import {moderateScale} from 'react-native-size-matters';
+	View,
+	Text,
+	StyleSheet,
+	FlatList,
+	ActivityIndicator,
+} from "react-native";
+import { moderateScale } from "react-native-size-matters";
 
-import Header from '../components/common/Header';
-import Constants from '../constants';
-import {getCall} from '../helpers/RestClient';
-import ApiUrls from '../constants/ApiUrls';
-import SongCard from '../components/SongCard';
+import Header from "../components/common/Header";
+import Constants from "../constants";
+import { getCall } from "../helpers/RestClient";
+import ApiUrls from "../constants/ApiUrls";
+import SongCard from "../components/SongCard";
 
 const SongsList = (props) => {
-  const {navigation} = props;
-  const [songsList, setSongs] = useState(null);
-  const [loader, setLoader] = useState(false);
+	const { navigation } = props;
+	const [songsList, setSongs] = useState(null);
+	const [loader, setLoader] = useState(false);
 
-  useEffect(() => {
-    callSongsApi();
-  }, []);
+	useEffect(() => {
+		callSongsApi();
+	}, []);
 
-  const callSongsApi = () => {
-    setLoader(true);
-    getCall(ApiUrls.getSongs)
-      .then((response) => {
-        console.log('All songs=>>', response?.results);
-        setTimeout(() => {
-          setLoader(false);
-          setSongs(response?.results);
-        }, 1000);
-      })
-      .catch((err) => {
-        console.log('Error', err);
-      });
-  };
+	const callSongsApi = () => {
+		setLoader(true);
+		getCall(ApiUrls.getSongs)
+			.then((response) => {
+				setTimeout(() => {
+					setLoader(false);
+					setSongs(response?.results);
+				}, 1000);
+			})
+			.catch((err) => {
+				console.log("Error", err);
+			});
+	};
 
-  const renderSongsList = ({item, index}) => {
-    return <SongCard item={item} index={index} navigation={navigation} />;
-  };
+	const renderSongsList = ({ item, index }) => {
+		return <SongCard item={item} index={index} navigation={navigation} />;
+	};
 
-  return (
-    <View style={Styles.container}>
-      <Header title="SONGS" statusBarType="light-content" />
-      <View style={{flex: 1}}>
-        {loader ? (
-          <View
-            style={{
-              height: moderateScale(500),
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}>
-            <ActivityIndicator size="large" color={Constants.Colors.Primary} />
-          </View>
-        ) : songsList && songsList.length ? (
-          <FlatList
-            data={songsList}
-            keyExtractor={(item, index) => index + 'unique'}
-            renderItem={renderSongsList}
-            onRefresh={callSongsApi}
-            refreshing={loader}
-          />
-        ) : (
-          <Text>No Songs Found!</Text>
-        )}
-      </View>
-    </View>
-  );
+	return (
+		<View style={Styles.container}>
+			<Header title="SONGS" statusBarType="light-content" />
+			<View style={{ flex: 1 }}>
+				{loader ? (
+					<View
+						style={{
+							height: moderateScale(500),
+							alignItems: "center",
+							justifyContent: "center",
+						}}
+					>
+						<ActivityIndicator
+							size="large"
+							color={Constants.Colors.Primary}
+						/>
+					</View>
+				) : songsList && songsList.length ? (
+					<FlatList
+						data={songsList}
+						keyExtractor={(item, index) => index + "unique"}
+						renderItem={renderSongsList}
+						onRefresh={callSongsApi}
+						refreshing={loader}
+					/>
+				) : (
+					<Text>No Songs Found!</Text>
+				)}
+			</View>
+		</View>
+	);
 };
 
 export default SongsList;
 
 const Styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: Constants.Colors.borderGrayColor,
-  },
+	container: {
+		flex: 1,
+		backgroundColor: Constants.Colors.borderGrayColor,
+	},
 });
